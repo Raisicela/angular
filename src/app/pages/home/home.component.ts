359a031963626a26c3112819ca5444b1b6c31a61
Raisa Guayasamin <raisa.marilyn1993@gmail.com>
2024-04-10 20:09:42 -0500

    Uso de ReactiveFormModule para agregar nueva tarea, editar tarea y tarea completada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../models/task.model';
+import { FormControl, ReactiveFormsModule, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -32,10 +33,22 @@ export class HomeComponent {
       completed: false
     }
   ])
-  changeHandler(event: Event) {
-    const input = event.target as HTMLInputElement;
-    const newTask = input.value
-    this.addTask(newTask)
+
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      // Validators.pattern('^\\S.*$'),
+    ]
+  })
+  changeHandler() {
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value.trim();
+      if (value !== '') {
+        this.addTask(value)
+        this.newTaskCtrl.setValue('')
+      }
+    }
   };
   addTask(title: string) {
     const newTask = {
@@ -55,6 +68,38 @@ export class HomeComponent {
           return {
             ...task,
             completed: !task.completed
+          }
+        }
+        return task
+      })
+    })
+  }
+
+  updateTaskEditingMode(index:number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            editing: true
+          }
+        }
+        return {
+          ...task,
+          editing: false
+        }
+      })
+    })
+  }
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            title: input.value,
+            editing:false
           }
         }
         return task
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
@@ -25,6 +26,24 @@ export class LabsComponent {
     age: 3,
     avatar: 'https://w3schools.com/howto/img_avatar.png'
   })
+
+  colorCtrl = new FormControl();
+  widthCtrl = new FormControl(50, {
+    nonNullable:true
+  })
+  nameCtrl = new FormControl('Raisa', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ]
+  })
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe(value => {
+      console.log(value)
+    })
+}
 
   clickHandler() {
     alert('Hola')
